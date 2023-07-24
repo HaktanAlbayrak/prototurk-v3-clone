bbb33f9d46ab90edff252795c3db3636f6dd409d
Haktan Albayrak <78109456+HaktanAlbayrak@users.noreply.github.com>
2023-07-25 00:20:50 +0300

    dark/light mode design - language system has been included in the project.

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import store from './stores';
 import routes from './routes';
 import './assets/css/style.css';
+import './utils/language';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
